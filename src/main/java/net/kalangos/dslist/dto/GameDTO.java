9dfc225584b15eb0bdb5799e720f2744ac0b0eb7
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-05-10 23:27:04 -0300

    GET game by id

--- src/main/java/net/kalangos/dslist/dto/GameDTO.java
+++ src/main/java/net/kalangos/dslist/dto/GameDTO.java
@@ -1,51 +1,27 @@
-package net.kalangos.dslist.entities;
+package net.kalangos.dslist.dto;
 
-import java.util.Objects;
+import org.springframework.beans.BeanUtils;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import net.kalangos.dslist.entities.Game;
 
-@Entity
-@Table(name="tb_game")
-public class Game {
+public class GameDTO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
-	
-	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
 	private String platforms;
 	private Double score;
 	private String imgUrl;
-	
-	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	
-	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
-	public Game() {
+	public GameDTO() {
 		
 	}
-
-	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
-			String shortDescription, String longDescription) {
-		this.id = id;
-		this.title = title;
-		this.year = year;
-		this.genre = genre;
-		this.platforms = platforms;
-		this.score = score;
-		this.imgUrl = imgUrl;
-		this.shortDescription = shortDescription;
-		this.longDescription = longDescription;
+	
+	public GameDTO(Game entity) {
+		BeanUtils.copyProperties(entity, this);
 	}
 
 	public Long getId() {
@@ -119,24 +95,6 @@ public class Game {
 	public void setLongDescription(String longDescription) {
 		this.longDescription = longDescription;
 	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Game other = (Game) obj;
-		return Objects.equals(id, other.id);
-	}
 	
 	
-
 }
--- src/main/java/net/kalangos/dslist/services/GameService.java
+++ src/main/java/net/kalangos/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import net.kalangos.dslist.dto.GameDTO;
 import net.kalangos.dslist.dto.GameMinDTO;
 import net.kalangos.dslist.entities.Game;
 import net.kalangos.dslist.repositories.GameRepository;
@@ -15,6 +17,14 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(result);
+		return dto;
+	}
+
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 	
 		List<Game> result = gameRepository.findAll();
